d335b3be794484eb7bca0e972e66aafbe3fe6b2e
Alpha787 <vasya22668@gmail.com>
2022-10-02 10:29:01 +0300

    Solving CW

---
+++ Square(n)_Sum.js
@@ -0,0 +1,19 @@
+function squareSum (numbers) {
+
+}
+
+
+
+
+
+
+
+
+
+descrive("Tests", () => {
+    it("test", () => {
+        Test.assertEquals(squareSum([1,2]), 5)
+        Test.assertEquals(squareSum([0, 3, 4, 5]), 50)
+        Test.assertEquals(square([]), 0)
+    });
+});
